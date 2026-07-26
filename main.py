f7618c5bc0a7b52c0973be52cc11b30d9e9ad482
Mustafa-Awd <mustafa.h.awad2005@gmail.com>
2026-07-26 21:02:54 +0300

    Complete Mission 2: inspect dataset

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-from src.analysis import load_data
-
-
+from src.analysis import load_data, inspect_data
 def main():
     print("Hello from student-grades-analysis!")
-    load_data()
 
+    load_data()
+    inspect_data()
 
 if __name__ == "__main__":
     main()
--- src/analysis.py
+++ src/analysis.py
@@ -1,13 +1,29 @@
 from pathlib import Path
 import pandas as pd
+from rich import print
+from rich.console import Console
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 DATA_PATH = BASE_DIR / "data" / "students.csv"
 
+data = pd.read_csv(DATA_PATH)
+console = Console()
 
 def load_data():
-    data = pd.read_csv(DATA_PATH)
-    print("======== Top 5 ============\n")
-    print(data.iloc[0:5, :])
-    print("======== Bottom 5 ============\n")
+    console.rule("Mission 1")
+    print("[green]======================== Top 5 ========================[/green]\n")
+    print(data.iloc[0:5, :], "\n")
+    print("[green]======================== Bottom 5 ========================[/green]\n")
     print(data.tail())
+
+    return data
+
+
+def inspect_data():
+    console.rule("Mission 2")
+    student_count = len(data)
+    column_count = len(data.columns)
+
+    print(f"\n[bold cyan]There are {student_count} students/rows and {column_count} columns.[/bold cyan] [red]No null values[/red]\n")
+    print(f"[green]column names[/green]: {list(data.columns)}")
+    print(f"[green]column types[/green]: {data.dtypes.astype(str).to_list()}")
